530533f6fce5779618320e2805a85711775c9d57
Jeff Wong <v-jwong1@expedia.com>
2020-06-12 21:40:26 +1000

    set up main App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://jeffs.xyz:11002/a")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.log(`Something went wrong! ${err}`))
+  }, []);
+
   return (
     <div>
-      Hello world!
+      <h2>Hello world!</h2>
+      {users.map(user => (
+        <div>
+          Id: {user.id}, Name: {user.name}
+        </div>
+      ))}
     </div>
   );
 };
