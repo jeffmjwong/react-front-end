1e19a7b4d8a17a14b735f99862c600fa78697c21
Jeff Wong <v-jwong1@expedia.com>
2020-10-13 20:39:12 +1000

    change port 80 to port 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 80;
+const port = 3001;
 
 app.use(express.static(path.resolve("dist", "www")));
 
