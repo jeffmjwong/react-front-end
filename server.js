636c168fc347e444e7c09ef03ecfe836103ae472
Jeff Wong <v-jwong1@expedia.com>
2020-10-13 17:35:20 +1000

    update server.js file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3001;
+const port = 80;
 
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve("dist", "www")));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
