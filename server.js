0f5eb2c9a5d3ceea45f5db23f37af2d469bbe263
Jeff Wong <v-jwong1@expedia.com>
2020-10-25 21:21:14 +1000

    Add endpoint /a in express server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
+const fetch = require("node-fetch").default;
 
 const app = express();
 const port = 3001;
 
 app.use(express.static(path.resolve("dist", "www")));
+
+app.get("/a", (req, res) => {
+  console.log("Hitting /a endpoint!");
+
+  fetch("http://api:3000/a")
+    .then(r => r.json())
+    .then(data => res.send(data))
+    .catch(err => console.log(`Something went wrong! ${err}`))
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
